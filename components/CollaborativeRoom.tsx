d2111a916be83a1369aa9ce6a14a0f237c48b696
Santhana Ganapathy <santhanaganapathy401@gmail.com>
2024-08-07 16:38:21 +0530

    Implement Share Modal

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from './ui/input';
 import Image from 'next/image';
 import { updateDocument } from '@/lib/actions/rooms.actions';
 import Loader from './Loader';
+import ShareModal from './ShareModal';
 
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType}: CollaborativeRoomProps) => {
@@ -107,6 +108,15 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType}: Coll
         <div className="flex w-full flex-1 justify-end gap-2
         sm:gap-3">
             <ActiveCollaborators />
+
+             <ShareModal
+               roomId = {roomId}
+               collaborators={users}
+               creatorId={roomMetadata.creatorId}
+               currentUserType={currentUserType}             
+             />
+                
+
             <SignedOut>
                <SignInButton />
             </SignedOut>
